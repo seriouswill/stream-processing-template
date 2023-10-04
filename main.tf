1e4d10a1abc63e089be81200e2ee6ea3e3ed55f3
Will O'Connell <williamjamesoc@gmail.com>
2023-10-04 12:58:21 +0100

    hiding hardcoded key names, allow dynamic repo access

--- main.tf
+++ main.tf
@@ -1,7 +1,17 @@
+variable "gh_user" {
+  description = "Name of key for GitHub user to access the url"
+  type        = string
+}
+
 variable "repo_url" {
   description = "The URL of the git repository to clone"
   type        = string
-  default     = "https://github.com/seriouswill/stream-processing-template.git"  # Optional default value
+  default     = "https://github.com/${var.gh_user}/stream-processing-template.git"  # Optional default value
+}
+
+variable "key_name" {
+  description = "Name of key for AWS access"
+  type        = string
 }
 
 provider "aws" {
@@ -11,7 +21,7 @@ provider "aws" {
 resource "aws_instance" "msk_client" {
   ami             = "ami-0b2287cff5d6be10f" # Amazon Linux 2 AMI (HVM) - Kernel 5.10, SSD Volume Type
   instance_type   = "t2.micro"
-  key_name        = "wills-monster-server-keys" # Ensure you have this key pair in AWS
+  key_name        = "${var.key_name}" # Ensure you have this key pair in AWS
   user_data = <<-EOF
               #!/bin/bash
               sudo yum update -y
@@ -28,12 +38,12 @@ resource "aws_instance" "msk_client" {
               
               
               # Clone the git repository
-              git clone ${var.repo_url} /home/ec2-user/repo-name
+              git clone ${var.repo_url} /home/ec2-user/stream-processing-template
               EOF
 
 
   tags = {
-    Name = "MSKClientInstance"
+    Name = "${var.git_username}MSKClient"
   }
 
     
